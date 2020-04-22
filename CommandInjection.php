2db5a471bf4c775203f445239fb5e9d30ae7f216
coloristlife <yany.dong@gmail.com>
2020-04-22 14:52:30 +0800

    Update CommandInjection.php

--- CommandInjection.php
+++ CommandInjection.php
@@ -1,4 +1,4 @@
 <?php
 $out=shell_execute($_REQUEST['cmd']);
-echo "<br><b>CMDi result for:</b>".$_REQUEST['cmd']."<br>".$out"</br>;
+echo "<br><b>CMDi result for:</b>".$_REQUEST['cmd']."<br>".$out"</br>";
 ?>
